e2f25e7dee59d685693d73842837d8de61ef05d1
arun902730 <arun.kumar_cs20@gla.ac.in>
2022-11-19 18:35:55 +0530

    update

--- js/firebaseConfig.js
+++ js/firebaseConfig.js
@@ -1,12 +1,13 @@
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyAAW9v-16c65uhim3H8Db0aB4jLegWsegA",
-    authDomain: "shopping-cart-e1b8a.firebaseapp.com",
-    databaseURL: "https://shopping-cart-e1b8a-default-rtdb.firebaseio.com",
-    projectId: "shopping-cart-e1b8a",
-    storageBucket: "shopping-cart-e1b8a.appspot.com",
-    messagingSenderId: "495964716232",
-    measurementId: "G-D6EEDW1NCS"
-  };
+  apiKey: "AIzaSyDCd-GU953Q3fVOd3a6lpNpw3wg2HfTxxA",
+  authDomain: "auth-f3976.firebaseapp.com",
+  databaseURL: "https://auth-f3976-default-rtdb.firebaseio.com",
+  projectId: "auth-f3976",
+  storageBucket: "auth-f3976.appspot.com",
+  messagingSenderId: "826229875652",
+  appId: "1:826229875652:web:38038e7ed81e4dd7e3ba91",
+  measurementId: "G-PD19591P9R"
+};
     firebase.initializeApp(firebaseConfig);
   
